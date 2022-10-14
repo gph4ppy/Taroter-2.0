9abdf3751ccfb3d96763a2ad26f9c745c1ce10ed
Jakub "GPH4PPY" Dąbrowski <jakub.dabrowski.dev@icloud.com>
2022-10-14 23:29:38 +0200

    Add TRColors

--- Taroter/Source Files/Application Flow/TaroterApp.swift
+++ Taroter/Source Files/Application Flow/TaroterApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct TaroterApp: App {
     var body: some Scene {
         WindowGroup {
-            CardsView()
+            NavigationView {
+                CardsView()
+            }
+            .navigationViewStyle(.stack)
         }
     }
 }
---
+++ Taroter/Source Files/Common/Models/TRColors.swift
@@ -0,0 +1,15 @@
+//
+//  TRColors.swift
+//  Taroter
+//
+//  Created by Jakub Dąbrowski on 14/10/2022.
+//
+
+import SwiftUI
+
+struct TRColor {
+    static let blackPearl   = Color("BlackPearl")
+    static let sangrina     = Color("Sangrina")
+    static let snow         = Color("Snow")
+    static let tyrianPurple = Color("TyrianPurple")
+}
