f1dd95402852c2facadd5e32aeca186ff36b8c11
Jakub "GPH4PPY" Dąbrowski <jakub.dabrowski.dev@icloud.com>
2022-10-14 23:19:51 +0200

    Move files to folders

---
+++ Taroter/Source Files/Application Flow/Cards/Views/CardsView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Taroter
+//
+//  Created by Jakub Dąbrowski on 14/10/2022.
+//
+
+import SwiftUI
+
+struct CardsView: View {
+    var body: some View {
+        Text("Cards View")
+            .font(.largeTitle)
+    }
+}
+
+struct CardsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardsView()
+    }
+}
